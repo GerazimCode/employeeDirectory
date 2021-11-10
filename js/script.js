5d80e7a8424dca6e6b33bb7022b9e7dc0ec66027
GerazimCode <gerazimcode@gmail.com>
2021-11-10 13:46:06 -0500

    add model box functionalities

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ async function getEmployees(url){
             employeeGallery(employeeList);
             // employeeModal(employeeList);
         })
+        // for later...add error methods to log if there is an error...
+        // look into the error message on the console.
 }
 
 // the functions shows employeers in the gallery by 12
@@ -38,6 +40,37 @@ function employeeGallery(data){
     })
 }
 
-getEmployees(api)
+// following function displays and closes the modal when an employee is clicked...
+
+function modalWindow(){
+    let box = `
+    <div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${employee.picture.medium}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+            <p class="modal-text">${employee.email}</p>
+            <p class="modal-text cap">${employee.location.city}</p>
+            <hr>
+            <p class="modal-text">${employee.phone}</p>
+            <p class="modal-text">${employee.location.stree.number} ${employee.location.street.name} ${employee.location.city} ${employee.location.state} ${employee.location.postcode}</p>
+            <p class="modal-text">Birthday: ${employee.dob.date.slice(5,7)}/ ${employee.dob.date.slice(8,10)}/ ${employee.dob.date.slice(0,4)} </p>
+        </div>
+    </div>` 
+
+    // adds the box to the body
+    gallery.insertAdjacentHTML("afterend", box);
+
+    // closes the box
+    let closeButton = document.getElementById("modal-close-btn");
+    let boxContainer = document.getElementsByClassName("modal-container");
+    closeButton.addEventListener("click", (e) => {
+        boxContainer.remove();
+    })
+
+}
+
+
 
 
