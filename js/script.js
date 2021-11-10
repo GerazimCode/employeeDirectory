ace0e96792ab32f51d93b890bb8c3684e88321d0
GerazimCode <gerazimcode@gmail.com>
2021-11-10 13:10:48 -0500

    add employee gallery functionality

--- js/script.js
+++ js/script.js
@@ -4,8 +4,40 @@ let gallery = document.querySelector("#gallery");
 let employeeList = [];
 let data;
 
-// api request 
-let api = ""
+// api url with 12 employees only and US nationality...
+let api = "https://randomuser.me/api/?results=12&nat=us";
+// console.log(fetch(api));
 
+// handles fetch requests and manupulates data from the api
+async function getEmployees(url){
+    let employeeResponse = await fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            employeeList = data.results;
+            employeeGallery(employeeList);
+            // employeeModal(employeeList);
+        })
+}
+
+// the functions shows employeers in the gallery by 12
+function employeeGallery(data){
+    data.map(employee => {
+        employee = `
+        <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${employee.picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+            <p class="card-text">${employee.email}</p>
+            <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+        </div>
+    </div>`
+
+    gallery.insertAdjacentHTML("beforeend", employee);
+    })
+}
+
+getEmployees(api)
 
 
