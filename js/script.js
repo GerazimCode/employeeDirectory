5157a46eb76a14f81f9d1d0817c719b315f5c3d4
GerazimCode <gerazimcode@gmail.com>
2021-11-10 11:53:28 -0500

    add url of api and starter variables

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// selecting elements from the DOM & starter variables...
+let searchContainer = document.querySelector(".search-container");
+let gallery = document.querySelector("#gallery");
+let employeeList = [];
+let data;
+
+// api request 
+let api = ""
+
+
+
